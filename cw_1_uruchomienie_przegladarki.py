f20c5c8dbb1e05fba14f36aa89580f8888fadd3d
piotr.w.dabrowski2@gmail.com <Piotr Dbrowski>
2024-03-09 10:44:37 +0100

    first commit selenium

---
+++ cw_1_uruchomienie_przegladarki.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import time
+
+# inicjalizacja przeglądarki
+driver = webdriver.Chrome()
+
+url = "https://www.google.com/"
+new_url = "https://www.wp.pl"
+
+# uruchomienie konkretnego adresu
+driver.get(url)
+
+# otwórz nowe okno
+driver.execute_script("window.open('');")
+
+# Opcjonalnie: przełącz się na nową zakładkę, jeśli chcesz z niej korzystać
+driver.switch_to.window(driver.window_handles[1])
+driver.get(new_url)
+
+# maksymalizacja okna
+driver.maximize_window()
+
+time.sleep(3)
+
+driver.set_window_size(800, 400)
+
+time.sleep(1)
+
+# zamknięcie przeglądarki -> jak zamknięcie poprzez X
+# driver.close() zamknięcie 1 okna.
+driver.quit()
+
